35474ea061b71f96459a20f30364216f352e1e4f
Saugat Thapa <saugat.thapa97@gmail.com>
2018-11-07 01:02:36 -0600

    draft

--- repos/CalendarApp/Controllers/HomeController.cs
+++ repos/CalendarApp/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace CalendarApp.Controllers
             return View();
         }
 
-       [Authorize]
+        [Authorize]
         public JsonResult GetEvents()
         {
             using (CalendarAppDatabaseEntities1 context = new CalendarAppDatabaseEntities1())
@@ -29,6 +29,55 @@ namespace CalendarApp.Controllers
                 var events = context.Events.ToList();
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
+        }
+        
+        [Authorize]
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+            using (CalendarAppDatabaseEntities1 dc = new CalendarAppDatabaseEntities1())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status } };
+        }
+
+        [Authorize]
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (CalendarAppDatabaseEntities1 dc = new CalendarAppDatabaseEntities1())
+            {
+                var v = dc.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status } };
         }
     }
 }
